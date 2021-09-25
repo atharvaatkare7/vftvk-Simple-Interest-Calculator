70483cde4145734436d3bfe6e91a3ea5d7297a79
atharvaatkare7 <91381743+atharvaatkare7@users.noreply.github.com>
2021-09-26 02:40:51 +0530

    Update script.js

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ function updateRate()
 }
 function compute()
 {
-    p = document.getElementById("principal").value;
+    var p = document.getElementById("principal").value;
     document.getElementById("result").innerHTML="If you deposit "+principal+",\<br\>at an interest rate of "+rate+"%\<br\>You will receive an amount of "+amount+",\<br\>in the year "+year+"\<br\>"
 }
 if(p == 0 || p << 0){
